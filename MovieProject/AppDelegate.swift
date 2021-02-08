daccb734b41f96cf3b946bf03d7022384fed0725
onurbasdas <onurebasdase@hotmail.com>
2021-02-08 16:50:56 +0300

    add movie list completed

--- MovieProject/AppDelegate.swift
+++ MovieProject/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UITableView.appearance().separatorStyle = .none
         // Override point for customization after application launch.
         return true
     }
--- MovieProject/Views/MovieListView.swift
+++ MovieProject/Views/MovieListView.swift
@@ -8,8 +8,80 @@
 import SwiftUI
 
 struct MovieListView: View {
+    
+    @ObservedObject private var nowPlayingState = MovieListState()
+    @ObservedObject private var upcomingState = MovieListState()
+    @ObservedObject private var topRatedState = MovieListState()
+    @ObservedObject private var popularState = MovieListState()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Group {
+                    if nowPlayingState.movies != nil {
+                        MoviePosterCarousel(title: "Now Playing", movies: nowPlayingState.movies!)
+                        
+                    } else {
+                        LoadingView(isLoading: self.nowPlayingState.isLoading, error: self.nowPlayingState.error) {
+                            self.nowPlayingState.loadMovies(with: .nowPlaying)
+                        }
+                    }
+                    
+                }
+                .listRowInsets(EdgeInsets(top: 16, leading: 0, bottom: 16, trailing: 0))
+                
+                Group {
+                    if upcomingState.movies != nil {
+                        MovieBackdropCarouselView(title: "Upcoming", movies: upcomingState.movies!)
+                    } else {
+                        LoadingView(isLoading: self.upcomingState.isLoading, error: self.upcomingState.error) {
+                            self.upcomingState.loadMovies(with: .upcoming)
+                        }
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                
+                Group {
+                    if topRatedState.movies != nil {
+                        MovieBackdropCarouselView(title: "Top Rated", movies: topRatedState.movies!)
+                        
+                    } else {
+                        LoadingView(isLoading: self.topRatedState.isLoading, error: self.topRatedState.error) {
+                            self.topRatedState.loadMovies(with: .topRated)
+                        }
+                    }
+                    
+                    
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
+                
+                Group {
+                    if popularState.movies != nil {
+                        MovieBackdropCarouselView(title: "Popular", movies: popularState.movies!)
+                        
+                    } else {
+                        LoadingView(isLoading: self.popularState.isLoading, error: self.popularState.error) {
+                            self.popularState.loadMovies(with: .popular)
+                        }
+                    }
+                }
+                .listRowInsets(EdgeInsets(top: 8, leading: 0, bottom: 16, trailing: 0))
+                
+                
+                
+            }
+            .navigationBarTitle("The Movie List")
+           
+        }
+        .onAppear {
+            self.nowPlayingState.loadMovies(with: .nowPlaying)
+            self.upcomingState.loadMovies(with: .upcoming)
+            self.topRatedState.loadMovies(with: .topRated)
+            self.popularState.loadMovies(with: .popular)
+        }
+        
     }
 }
 
