c14518cd18116882b8fd60400930f59b0f02bd8d
onurbasdas <onurebasdase@hotmail.com>
2021-02-08 13:41:40 +0300

    add image loader completed

---
+++ MovieProject/Views/MovieBackdropCard.swift
@@ -0,0 +1,20 @@
+//
+//  MovieBackdropCard.swift
+//  MovieProject
+//
+//  Created by Onur Başdaş on 8.02.2021.
+//
+
+import SwiftUI
+
+struct MovieBackdropCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieBackdropCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCard()
+    }
+}
