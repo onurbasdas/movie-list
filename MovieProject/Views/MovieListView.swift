90f38aa4273fdcc0a6bba5f178ab8aa9ce37505d
onurbasdas <onurebasdase@hotmail.com>
2021-02-08 16:36:40 +0300

    add loading view completed

---
+++ MovieProject/Views/MovieListView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieListView.swift
+//  MovieProject
+//
+//  Created by Onur Başdaş on 8.02.2021.
+//
+
+import SwiftUI
+
+struct MovieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieListView()
+    }
+}
