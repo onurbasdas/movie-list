8bba5564d2fc055be2036c02f0e3a0cd7d047f30
onurbasdas <42867363+onurbasdas@users.noreply.github.com>
2021-02-09 00:55:02 +0300

    Update MovieStore.swift

--- MovieProject/Services/MovieStore.swift
+++ MovieProject/Services/MovieStore.swift
@@ -13,7 +13,7 @@ class MovieStore: MovieService {
     static let shared = MovieStore()
     private init() {}
     
-    private let apiKey = "9a2051f6f3d245541ab63c2b7d1c0bcd"
+    private let apiKey = "APIKEY"
     private let baseAPIURL = "https://api.themoviedb.org/3"
     private let urlSession = URLSession.shared
     private let jsonDecoder = Utils.jsonDecoder
